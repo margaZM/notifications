f68f7f9dceb7e04fd1b942455244e768e09f9fbb
margazm <aurazambrano227@gmail.com>
2026-01-24 20:31:03 -0500

    docs: add badge circle ci

--- up.test.js
+++ up.test.js
@@ -1,7 +1,10 @@
 const { execSync } = require('child_process');
 const path = require('path');
 
-execSync('docker compose -f docker-compose.test.yml up --build', {
-	cwd: path.join(__dirname, 'backend'),
-	stdio: 'inherit',
-});
+execSync(
+	'docker compose -f docker-compose.test.yml up --build --exit-code-from notifications-service',
+	{
+		cwd: path.join(__dirname, 'backend'),
+		stdio: 'inherit',
+	},
+);
