329d2176dddeccdc46dbddd2dee337bfd43e720e
margazm <aurazambrano227@gmail.com>
2026-01-24 22:13:25 -0500

    tests: stop last container when finished all tests

--- up.test.js
+++ up.test.js
@@ -1,10 +1,40 @@
-const { execSync } = require('child_process');
+const { execSync, spawn } = require('child_process');
 const path = require('path');
 
-execSync(
-	'docker compose -f docker-compose.test.yml up --build --exit-code-from notifications-service',
-	{
-		cwd: path.join(__dirname, 'backend'),
+const backendPath = path.join(__dirname, 'backend');
+
+try {
+	execSync('docker compose -f docker-compose.test.yml up --build -d', {
+		cwd: backendPath,
 		stdio: 'inherit',
-	},
-);
+	});
+	const logs = spawn(
+		'docker',
+		['compose', '-f', 'docker-compose.test.yml', 'logs', '-f'],
+		{
+			cwd: backendPath,
+			stdio: 'inherit',
+		},
+	);
+	const lastContainerId = execSync(
+		'docker compose -f docker-compose.test.yml ps notifications-service -q',
+		{
+			cwd: backendPath,
+			encoding: 'utf-8',
+		},
+	).trim();
+	const exitCode = execSync(`docker wait ${lastContainerId}`, {
+		encoding: 'utf-8',
+	}).trim();
+
+	logs.kill();
+	if (exitCode !== '0') process.exit(1);
+} catch (error) {
+	console.error('Error:', error.message);
+	process.exit(1);
+} finally {
+	execSync('docker compose -f docker-compose.test.yml down -v --remove-orphans', {
+		cwd: backendPath,
+		stdio: 'inherit',
+	});
+}
