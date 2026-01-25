968db4495924ceae39f0bfd5a1b47f3fbb4d6285
margazm <aurazambrano227@gmail.com>
2026-01-25 13:39:08 -0500

    test: new up test

--- up.test.js
+++ up.test.js
@@ -4,28 +4,13 @@ const path = require('path');
 const backendPath = path.join(__dirname, 'backend');
 
 try {
-	execSync('docker compose -f docker-compose.test.yml up --build -d', {
-		cwd: backendPath,
-		stdio: 'inherit',
-	});
-	const logs = spawn(
-		'docker',
-		['compose', '-f', 'docker-compose.test.yml', 'logs', '-f'],
-		{
-			cwd: backendPath,
-			stdio: 'inherit',
-		},
-	);
 	execSync(
-		'docker compose -f docker-compose.test.yml up --exit-code-from notifications-service',
+		'docker compose -f docker-compose.test.yml up --build --no-abort-on-container-exit --exit-code-from notifications-service',
 		{
 			cwd: backendPath,
 			stdio: 'inherit',
 		},
 	);
-	logs.kill();
 } catch (error) {
 	process.exit(1);
 }
-// NOTA: NO agregues el cleanup aquí (el "down -v"),
-// deja que CircleCI lo haga después de subir a Coveralls.
