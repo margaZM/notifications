e579cdd7b3aaa243978a1801e41f66bf96432240
margazm <aurazambrano227@gmail.com>
2026-02-06 23:05:57 -0500

    build: add FRONTEND_URL

--- backend/apps/api-gateway/src/main.ts
+++ backend/apps/api-gateway/src/main.ts
@@ -1,58 +1,58 @@
-import 'dotenv/config';
-import { NestFactory } from '@nestjs/core';
-import { ApiGatewayModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { Logger, ValidationPipe } from '@nestjs/common';
-import { RpcExceptionFilter } from './modules/common/filters/rpc-exception.filter';
+import "dotenv/config";
+import { NestFactory } from "@nestjs/core";
+import { ApiGatewayModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { Logger, ValidationPipe } from "@nestjs/common";
+import { RpcExceptionFilter } from "./modules/common/filters/rpc-exception.filter";
 
 async function bootstrap() {
-	const app = await NestFactory.create(ApiGatewayModule);
+  const app = await NestFactory.create(ApiGatewayModule);
 
-	app.useGlobalFilters(new RpcExceptionFilter());
+  app.useGlobalFilters(new RpcExceptionFilter());
 
-	const logger = new Logger('Gateway');
+  const logger = new Logger("Gateway");
 
-	app.enableCors({
-		origin: ['http://localhost:4000'],
-		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-		credentials: true,
-		allowedHeaders: 'Content-Type, Accept, Authorization',
-	});
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || "http://localhost:4000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+    allowedHeaders: "Content-Type, Accept, Authorization",
+  });
 
-	const config = new DocumentBuilder()
-		.setTitle('Notifications API')
-		.setDescription('Sistema de gestion de notificaciones.')
-		.setVersion('1.0')
-		.addBearerAuth(
-			{
-				type: 'http',
-				scheme: 'bearer',
-				bearerFormat: 'JWT',
-				name: 'JWT',
-				description: 'JWT token',
-				in: 'header',
-			},
-			'access-token',
-		)
-		.build();
+  const config = new DocumentBuilder()
+    .setTitle("Notifications API")
+    .setDescription("Sistema de gestion de notificaciones.")
+    .setVersion("1.0")
+    .addBearerAuth(
+      {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+        name: "JWT",
+        description: "JWT token",
+        in: "header",
+      },
+      "access-token",
+    )
+    .build();
 
-	const documentFactory = () => SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('docs', app, documentFactory);
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, documentFactory);
 
-	app.useGlobalPipes(
-		new ValidationPipe({
-			whitelist: true,
-			transform: true,
-			validationError: {
-				target: false,
-				value: false,
-			},
-		}),
-	);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      validationError: {
+        target: false,
+        value: false,
+      },
+    }),
+  );
 
-	const port = process.env.port || 3000;
+  const port = process.env.port || 3000;
 
-	await app.listen(port);
-	logger.log(`API Gateway corriendo en http://localhost:${port}`);
+  await app.listen(port);
+  logger.log(`API Gateway corriendo en http://localhost:${port}`);
 }
 bootstrap();
