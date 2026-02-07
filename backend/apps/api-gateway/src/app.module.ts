053d9f1199ff39bd590d607f2ce87b2f253adb16
margazm <aurazambrano227@gmail.com>
2026-02-06 23:35:53 -0500

    build: update render.yaml

--- backend/apps/api-gateway/src/app.module.ts
+++ backend/apps/api-gateway/src/app.module.ts
@@ -4,12 +4,7 @@ import { ApiGatewayService } from "./app.service";
 import { ClientsModule, Transport } from "@nestjs/microservices";
 import { AuthController } from "./modules/auth/auth.controller";
 import { ContactController } from "./modules/contacts/contacts.controller";
-import {
-  AUTH_SERVICE,
-  CONTACTS_SERVICE,
-  NOTIFICATIONS_SERVICE,
-  SERVICES_CONFIG,
-} from "@margazm/common";
+import { AUTH_SERVICE, CONTACTS_SERVICE, NOTIFICATIONS_SERVICE } from "@margazm/common";
 import { JwtModule } from "@nestjs/jwt";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { NotificationController } from "./modules/notifications/notification.controller";
@@ -24,34 +19,42 @@ import { NotificationController } from "./modules/notifications/notification.con
         secret: config.get<string>("JWT_SECRET"),
       }),
     }),
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: AUTH_SERVICE,
-        transport: Transport.TCP,
-        options: {
-          host: "auth-service",
-          port: SERVICES_CONFIG.AUTH.PORT,
-        },
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: config.get<string>("AUTH_SERVICE_HOST") || "auth-service",
+            port: config.get<number>("AUTH_SERVICE_PORT") || 3001,
+          },
+        }),
       },
-    ]),
-    ClientsModule.register([
       {
         name: CONTACTS_SERVICE,
-        transport: Transport.TCP,
-        options: {
-          host: "contacts-service",
-          port: SERVICES_CONFIG.CONTACTS.PORT,
-        },
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: config.get<string>("CONTACTS_SERVICE_HOST") || "contacts-service",
+            port: config.get<number>("CONTACTS_SERVICE_PORT") || 3002,
+          },
+        }),
       },
-    ]),
-    ClientsModule.register([
       {
         name: NOTIFICATIONS_SERVICE,
-        transport: Transport.TCP,
-        options: {
-          host: "notifications-service",
-          port: SERVICES_CONFIG.NOTIFICATIONS.PORT,
-        },
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: config.get<string>("NOTIFICATIONS_SERVICE_HOST") || "notifications-service",
+            port: config.get<number>("NOTIFICATIONS_SERVICE_PORT") || 3003,
+          },
+        }),
       },
     ]),
   ],
